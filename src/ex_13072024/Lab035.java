3f8169cb7737ec4042a9bcd10234657de981dfe1
Shrutika <shrutika.nampalli@gmail.com>
2024-07-19 17:33:26 +0530

    commit and push files.

---
+++ src/ex_13072024/Lab035.java
@@ -0,0 +1,17 @@
+package ex_13072024;
+
+public class Lab035 {
+    public static void main(String[] args) {
+        // Escape Chars
+        char c = '\n'; // New line / Next Line
+        char c1 = '\t'; // Add Tab
+        char c2 = '\b'; // Backlash, delete 1 char
+        char c3 = '\r'; // Backlash, delete 1 word
+//        System.out.println("Shruti" + c3 + "Nampalli");
+//        System.out.println("Shruti" + c2 + "Nampalli");
+//        System.out.println("Shruti" + c1 + "Nampalli");
+        System.out.println("Shruti" + c + "Nampalli");
+
+
+    }
+}
---
+++ src/ex_13072024/Lab043.java
@@ -0,0 +1,15 @@
+package ex_13072024;
+
+public class Lab043 {
+    public static void main(String[] args) {
+        // Relational Operators -> boolean
+        //    // >, < >=,<=, == , != ( ! = ) - true or false.
+        int age_Shrutika = 25;
+        int age_Vicky = 30;
+//        boolean result = age_Vicky > age_Shrutika;
+//        boolean result = age_Vicky < age_Shrutika;
+        boolean result2 = age_Vicky >= age_Shrutika; // OR gate
+        System.out.println(result2);
+
+    }
+}
